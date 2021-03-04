87d56a26c79881e4a45fc4b2117b413727015763
Maanan <maanan.seduk@gmail.com>
2021-03-04 20:23:43 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+//VIDEO A 4:25
+
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+
+//load static assets
+app.use('/static',express.static(path.join(__dirname, 'public')))
+
+
+//home route
+app.get('/',(req, res) =>{
+  res.render('base', {title: 'Login System'})
+})
+
+
+
+app.listen(port, ()=> {
+  console.log(` listening ${port}`)
+})
